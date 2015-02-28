1c59628cdfc61ceaecff80f119634f93bf901912
Behzadkhosravifar <Behzad.khosravifar@gmail.com>
2015-02-28 14:34:30 +0330

    Restore Point and fix some bugs

--- Test Projects/Test All Projects/TestErrorHandlerBySelf/Program.cs
+++ Test Projects/Test All Projects/TestErrorHandlerBySelf/Program.cs
@@ -17,7 +17,8 @@ namespace TestErrorHandlerBySelf
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            ExpHandlerEngine.Start(new Connection("localhost", "UsersManagements"));
+            ExpHandlerEngine.Start(new Connection("localhost", "UsersManagements"),
+                ExceptionHandlerOption.Default | ExceptionHandlerOption.ReSizeSnapshots);
 
             Application.Run(new FormTest());
         }

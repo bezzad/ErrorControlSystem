51719c0c8ba22948e56c9664c232e538104cac2c
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2015-03-25 20:13:39 +0430

    ErrorHandlerEngineAdapterPattern document added

---
+++ Documentation Project/ErrorHandlerEngineAdapterPattern/ThisDocument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Tools.Word;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ErrorHandlerEngineAdapterPattern
+{
+    public partial class ThisDocument
+    {
+        private void ThisDocument_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisDocument_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisDocument_Startup);
+            this.Shutdown += new System.EventHandler(ThisDocument_Shutdown);
+        }
+
+        #endregion
+    }
+}

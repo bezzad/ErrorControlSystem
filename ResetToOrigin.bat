232d3deb8d594f5e9f8ed7e225413c805726a85b
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-03-29 12:45:31 +0430

    ResetToOrigin.bat and UpdateFromOrigin.bat upload

---
+++ ResetToOrigin.bat
@@ -0,0 +1,5 @@
+git fetch
+git reset --hard origin/master
+echo. 
+echo Reset to origin completed
+pause
---
+++ UpdateFromOrigin.bat
@@ -0,0 +1,7 @@
+git checkout master
+git fetch
+git diff origin/master
+git pull
+echo. 
+echo Updated Complete
+pause

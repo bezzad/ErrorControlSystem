e0af31f83691902f6df8a2845521ea82ba97b9a9
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-04-10 11:42:33 +0430

    Add signing file and another solution items

--- buildx86.bat
+++ buildx86.bat
@@ -2,3 +2,4 @@ del msbuild.log
 mkdir out
 
 "C:\Program Files\MSBuild\12.0\Bin\MSBuild.exe" "src\ErrorControlSystem.sln" /t:Clean /t:Rebuild /property:Configuration=Release /fileLogger /verbosity:normal
+pause

db523e291ef55b29cdd90ce2d89306e90f187866
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-04-19 08:52:01 +0430

    Update UpdateFromOrigin.bat

--- UpdateFromOrigin.bat
+++ UpdateFromOrigin.bat
@@ -1,5 +1,23 @@
+@echo off
+Echo Start Updating ...
+
 git checkout master
 git fetch
 git status
 git pull
+
+Echo Update Completed.
+pause
+
+Echo Start Building Project ...
+
+mkdir out
+
+IF EXIST "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" (
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "src\ErrorControlSystem.sln" /t:clean /t:Rebuild /property:Configuration=Release /verbosity:minimal
+) ELSE (
+"C:\Program Files\MSBuild\12.0\Bin\MSBuild.exe" "src\ErrorControlSystem.sln" /t:clean /t:Rebuild /property:Configuration=Release /verbosity:minimal
+)
+
+Echo Build Completed.
 pause

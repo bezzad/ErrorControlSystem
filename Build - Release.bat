931870a87d1c2dcfd7a66461164f5e2f2175cf91
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-04-20 18:19:11 +0430

    Build Release Command Codes Changed

--- Build - Release.bat
+++ Build - Release.bat
@@ -1,16 +1,4 @@
-@echo off
-Echo Start Updating ...
-
-git checkout master
-git fetch
-git status
-git pull
-
-Echo Update Completed.
-
-Echo ---------------------------------------------
-
-Echo Start Building Project ...
+@echo off 
 
 IF NOT "%VS110COMNTOOLS%" == "" (call "%VS110COMNTOOLS%vsvars32.bat")
 IF NOT "%VS120COMNTOOLS%" == "" (call "%VS120COMNTOOLS%vsvars32.bat")
@@ -18,6 +6,4 @@ IF NOT "%VS130COMNTOOLS%" == "" (call "%VS130COMNTOOLS%vsvars32.bat")
 IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 
 for /F %%A in ('dir /b src\*.sln') do call devenv src\%%A /Rebuild "Release" 
-
-Echo ---------------------------------------------
 pause
